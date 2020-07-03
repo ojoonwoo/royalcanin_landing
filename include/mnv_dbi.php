af18736bf49e82b28c5fd17b46cc7f4d9d772d7d
kyhfan <kyhfan@gmail.com>
2020-07-03 11:46:43 +0900

    00000

--- include/mnv_dbi.php
+++ include/mnv_dbi.php
@@ -9,7 +9,7 @@ class mnv_dbi
     var $my_db;
     public function Connect_MySQL()
     {
-        $my_db = new mysqli("localhost", "root", "alslqj~1", "royalcanin_dog");
+        $my_db = new mysqli("localhost", "root", "alslqj~1", "royalcanin_cat");
 
         if (mysqli_connect_error()) {
             exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
