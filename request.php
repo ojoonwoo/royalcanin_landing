fe65a0a01d6ce3bfd2f4dd87622cab7f5eddcf8b
kyhfan <kyhfan@gmail.com>
2020-07-10 18:07:20 +0900

    000000

--- request.php
+++ request.php
@@ -38,65 +38,67 @@
             </div>
         </div>
         <div class="content _sub __request">
-            <div class="sub-header">
-                <a href="javascript:void(0)" id="go-before"></a>
-                <a href="./" id="go-index"></a>
-            </div>
-            <div class="title-block">
-                <div class="prj-title">
-                    <img src="./images/project_logo.svg" class="project-logo" alt="고양이 주치의 프로젝트">
-                    <span class="text">
-                        <em>주치의</em><img src="./images/icon_power.png" alt="력" class="icon"><em>테스트</em></span>
+            <div class="inner">
+                <div class="sub-header">
+                    <a href="javascript:void(0)" id="go-before"></a>
+                    <a href="./" id="go-index"></a>
                 </div>
-                <div class="subject">
-                    <b><?=$cat_info['mb_cat_name']?>의 건강검진권 신청하기</b>
-                </div>
-            </div>
-            <div class="location-selector">
-                <button type="button" class="loc-trigger is-active" data-loc="central">
-                    <img src="./images/icon_map_central.png" alt="수도권" class="map">
-                    <p>서울, 경기, 인천 지역</p>
-                </button>
-                <button type="button" class="loc-trigger" data-loc="other">
-                    <img src="./images/icon_map_other.png" alt="수도권 외 지역" class="map">
-                    <p>서울, 경기, 인천<br><b>그 외 지역</b></p>
-                </button>
-            </div>
-            <div class="input-wrapper">
-                <p class="tip">
-                    <span>병원선택 tip</span><em>보다 편안한 반려묘와의 병원 방문을 위해 가까운 병원을 추천합니다.</em>
-                </p>
-                <div class="row">
-                    <div class="input-group _addr">
-                        <label for="sido">동물병원 찾기</label>
-                        <select id="sido" class="select-box">
-                            <option value="" selected>시/도</option>
-                        </select>
-                        <select id="sigugun" class="select-box">
-                            <option value="" selected>시/구/군</option>
-                        </select>
-                        <button type="button" id="addr-search"><span class="for-a11y">찾기</span></button>
+                <div class="title-block">
+                    <div class="prj-title">
+                        <img src="./images/project_logo.svg" class="project-logo" alt="고양이 주치의 프로젝트">
+                        <span class="text">
+                            <em>주치의</em><img src="./images/icon_power.png" alt="력" class="icon"><em>테스트</em></span>
+                    </div>
+                    <div class="subject">
+                        <b><?=$cat_info['mb_cat_name']?>의 건강검진권 신청하기</b>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="input-group">
-                        <input type="text" class="input-text" id="req-addr" placeholder="내가 선택한 병원" readonly>
-                        <p class="guide-msg">* 건강검진권, 헤마츄리아 당첨 시 선택한 동물병원에서만 검진 및 수령이 가능하며 변경이 불가하니 신중하게 선택해주세요.</p>
-                    </div>
+                <div class="location-selector">
+                    <button type="button" class="loc-trigger is-active" data-loc="central">
+                        <img src="./images/icon_map_central.png" alt="수도권" class="map">
+                        <p>서울, 경기, 인천 지역</p>
+                    </button>
+                    <button type="button" class="loc-trigger" data-loc="other">
+                        <img src="./images/icon_map_other.png" alt="수도권 외 지역" class="map">
+                        <p>서울, 경기, 인천<br><b>그 외 지역</b></p>
+                    </button>
                 </div>
-                <div class="row">
-                    <div class="input-group">
-                        <label for="req-name">보호자 이름</label>
-                        <input type="text" class="input-text" id="req-name" placeholder="">
+                <div class="input-wrapper">
+                    <p class="tip">
+                        <span>병원선택 tip</span><em>보다 편안한 반려묘와의 병원 방문을 위해 가까운 병원을 추천합니다.</em>
+                    </p>
+                    <div class="row">
+                        <div class="input-group _addr">
+                            <label for="sido">동물병원 찾기</label>
+                            <select id="sido" class="select-box">
+                                <option value="" selected>시/도</option>
+                            </select>
+                            <select id="sigugun" class="select-box">
+                                <option value="" selected>시/구/군</option>
+                            </select>
+                            <button type="button" id="addr-search"><span class="for-a11y">찾기</span></button>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="input-group _tel">
-                        <label for="req-phone1">휴대 전화번호</label>
-                        <input type="text" class="input-text" id="req-phone1" placeholder="">
-                        <input type="text" class="input-text" id="req-phone2" placeholder="">
-                        <input type="text" class="input-text" id="req-phone3" placeholder="">
-                        <p class="guide-msg">* 본 무료 건강검진권과 헤마츄리아는 추첨을 통해 제공되며, 추첨은 별개로 진행됩니다.</p>
+                    <div class="row">
+                        <div class="input-group">
+                            <input type="text" class="input-text" id="req-addr" placeholder="내가 선택한 병원" readonly>
+                            <p class="guide-msg">* 건강검진권, 헤마츄리아 당첨 시 선택한 동물병원에서만 검진 및 수령이 가능하며 변경이 불가하니 신중하게 선택해주세요.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-group">
+                            <label for="req-name">보호자 이름</label>
+                            <input type="text" class="input-text" id="req-name" placeholder="">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-group _tel">
+                            <label for="req-phone1">휴대 전화번호</label>
+                            <input type="text" class="input-text" id="req-phone1" placeholder="">
+                            <input type="text" class="input-text" id="req-phone2" placeholder="">
+                            <input type="text" class="input-text" id="req-phone3" placeholder="">
+                            <p class="guide-msg">* 본 무료 건강검진권과 헤마츄리아는 추첨을 통해 제공되며, 추첨은 별개로 진행됩니다.</p>
+                        </div>
                     </div>
                 </div>
             </div>
