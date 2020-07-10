be980406d102bbff63d7cd7e07e03bd352f04386
kyhfan <kyhfan@gmail.com>
2020-07-10 18:04:17 +0900

    00000

--- request.php
+++ request.php
@@ -14,6 +14,29 @@
 ?>
 <body>
     <div id="container">
+        <div id="header">
+            <div class="inner">
+                <a href="./" class="logo">
+                    <img src="./images/logo.png" alt="로얄캐닌 홈으로">
+                </a>
+                <nav class="menu">
+                    <ul>
+                        <li class="active">
+                            <a href="javascript:void(0)" data-url="#section1">메인</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)" data-url="#section2">주치의 프로젝트</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)" data-url="#section3">주치의력 테스트</a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0)" data-url="#section4">주치의력 업그레이드 TIPS</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
         <div class="content _sub __request">
             <div class="sub-header">
                 <a href="javascript:void(0)" id="go-before"></a>
