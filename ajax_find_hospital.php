7239021646070c8730e167bf4a49de2db0c0582e
kyhfan <kyhfan@gmail.com>
2020-07-13 14:24:57 +0900

    000000

---
+++ ajax_find_hospital.php
@@ -0,0 +1,15 @@
+<?php
+    include_once "./include/autoload.php";
+    $mnv_f 		= new mnv_function();
+    $my_db      = $mnv_f->Connect_MySQL();
+
+    $sido       = $_REQUEST['sido'];
+    $sigungu    = $_REQUEST['sigungu'];
+    $query = "SELECT hospital_name, hospital_addr FROM hospital_list WHERE 1 AND hospital_addr = '".$sido." ".$sigungu."'";
+    $result = mysqli_query($my_db, $query);
+
+    while($data = mysqli_fetch_array($result)) {
+        "<li><button type='button' class='hospi-trigger'><div><span class='chk-shape'></span></div><div><p class='h-name'>".$data['hospital_name']."</p><p class='h-addr'>".$data['hospital_addr']."</p></div></button></li>";
+
+    }
+?>
