c44bff4acf900a7a77fc47178e53ef574b63c2eb
kyhfan <kyhfan@gmail.com>
2020-07-14 17:27:40 +0900

    000000

--- index_cat.php
+++ index_cat.php
@@ -421,11 +421,12 @@
                 $(".menu li").removeClass("active");
                 $('.menu li').eq(1).addClass("active");
             } else if(curTop >= $('#section3').offset().top-headerHeight && curTop < $('#section4').offset().top-headerHeight - ($(window).height()-($('#section4').height()+$('#footer').height()))) {
+                console.log("scroll3");
                 // 스크롤 현재 위치 섹션 3
                 $(".menu li").removeClass("active");
                 $('.menu li').eq(2).addClass("active");
             } else  {
-                console.log("scroll");
+                console.log("scroll4");
                 // 스크롤 현재 위치 섹션 4
                 $(".menu li").removeClass("active");
                 $('.menu li').eq(3).addClass("active");
