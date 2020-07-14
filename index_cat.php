54da85ba9c78bfa36e5682dfc8f30245daef1f60
kyhfan <kyhfan@gmail.com>
2020-07-14 10:24:36 +0900

    00000

--- index_cat.php
+++ index_cat.php
@@ -109,18 +109,21 @@
                                     <button type="button" class="tab-trigger is-active" data-key="3_6h0o-t3Vw">
                                         <img src="./images/infl_tab_thumb_01_active.png" alt="" class="thumb">
                                         <span>프로 집사의 #주치의 프로젝트</span>
+                                        <img src="./images/main_02_play_btn_active.png" alt="" class="play-btn">
                                     </button>
                                 </li>
                                 <li>
                                     <button type="button" class="tab-trigger" data-key="CAInhDnQFaA">
                                         <img src="./images/infl_tab_thumb_02_common.png" alt="" class="thumb">
                                         <span>예민보스 집사의 #주치의 프로젝트</span>
+                                        <img src="./images/main_02_play_btn.png" alt="" class="play-btn">
                                     </button>
                                 </li>
                                 <li>
                                     <button type="button" class="tab-trigger" data-key="NWROQ1tCFPM">
                                         <img src="./images/infl_tab_thumb_03_common.png" alt="" class="thumb">
                                         <span>현명 집사의 #주치의 프로젝트</span>
+                                        <img src="./images/main_02_play_btn.png" alt="" class="play-btn">
                                     </button>
                                 </li>
                             </ul>
