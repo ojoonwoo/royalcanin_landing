b4666f924631e31b9ad7b12249a23f690b6f4b8e
kyhfan <kyhfan@gmail.com>
2020-07-16 16:30:14 +0900

    000000

--- head.php
+++ head.php
@@ -21,8 +21,8 @@
 		<link rel="stylesheet" href="./css/font.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css">
 		<link rel="stylesheet" href="./css/style.css?ver=2006291519">
-        <!-- <script src="./js/jquery-3.3.1.min.js"></script> -->
-        <script src="./js/jquery-1.11.2.min.js"></script>
+        <script src="./js/jquery-3.3.1.min.js"></script>
+        <!-- <script src="./js/jquery-1.11.2.min.js"></script> -->
         <!-- <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script> -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.1/gsap.min.js"></script>
 		<script src="./js/main.js"></script>
