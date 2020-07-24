a0f016370e98fa27a6f03dbb7bd42f6a4d4e7498
kyhfan <kyhfan@gmail.com>
2020-07-24 11:52:10 +0900

    00000

--- index_cat.php
+++ index_cat.php
@@ -353,7 +353,7 @@
             videoId: '3_6h0o-t3Vw',
             playerVars: {'enablejsapi': 1, 'autoplay': 0, 'controls': 1, 'rel': 0, 'loop': 1, 'origin': window.location.href, 'playsinline': 1, 'widget_refferer:': window.location.href},
             events: {
-                'onReady': onPlayerReady,
+                // 'onReady': onPlayerReady,
                 'onStateChange': onPlayerStateChange
             }
         });
