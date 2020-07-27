164093f95751e11d921ee7765b18a9021f038c2f
kyhfan <kyhfan@gmail.com>
2020-07-27 17:49:01 +0900

    000000

--- ajax_find_hospital3.php
+++ ajax_find_hospital3.php
@@ -18,6 +18,12 @@
     $return_arr = [];
     $i=0;
     while($data = mysqli_fetch_array($result)) {
+        if(strpos($data['hospital_addr4'], $data['hospital_addr3']) !== false) {  
+            $addr = $data['hospital_addr1']." ".$data['hospital_addr2']." ".$data['hospital_addr4'];
+        } else {  
+            $addr = $data['hospital_addr1']." ".$data['hospital_addr2']." ".$data['hospital_addr3']." ".$data['hospital_addr4'];
+        }  
+                
         $html .= "<li><button type='button' class='hospi-trigger' data-code='".$data['hospital_code']."'><div><span class='chk-shape'></span></div><div><p class='h-name'>".$data['hospital_name']."</p><p class='h-addr'>".$data['hospital_addr1']." ".$data['hospital_addr2']." ".$data['hospital_addr4']."</p></div></button></li>";
         $i++;
     }
