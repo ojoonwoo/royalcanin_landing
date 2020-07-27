a84e8424022b534b53ccffbb118fd2fcec894b80
kyhfan <kyhfan@gmail.com>
2020-07-27 15:27:49 +0900

    000000

--- request.php
+++ request.php
@@ -294,7 +294,7 @@
                 }
 
                 $.ajax({
-                    url: "./ajax_find_hospital2.php",
+                    url: "./ajax_find_hospital.php",
                     type: 'POST',
                     data: {
                         "sido"       : $("#sido").val(),
