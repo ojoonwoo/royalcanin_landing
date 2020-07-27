5f4f7479fe1963550cd776ea00f879bc81d73465
kyhfan <kyhfan@gmail.com>
2020-07-27 15:30:01 +0900

    Merge https://github.com/ojoonwoo/royalcanin_cat

--- request.php
+++ request.php
@@ -294,7 +294,7 @@
                 }
 
                 $.ajax({
-                    url: "./ajax_find_hospital.php",
+                    url: "./ajax_find_hospital2.php",
                     type: 'POST',
                     data: {
                         "sido"       : $("#sido").val(),
