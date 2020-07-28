75aa55e2ff692bc4897cde3dc26a82faf41a406d
ojoonwoo <ojoonwoo@gmail.com>
2020-07-28 16:06:55 +0900

    0728 수정

--- index.php
+++ index.php
@@ -408,8 +408,8 @@
                 }, 200);
             }
 
-            gtag('event', '인플루언서', {'event_category': '메인페이지', 'event_label': '프로 집사'});
-            gtag('event', 'TIPS', {'event_category': '메인페이지', 'event_label': '동물병원 쉽게 데려가는 방법'});
+            // gtag('event', '인플루언서', {'event_category': '메인페이지', 'event_label': '프로 집사'});
+            // gtag('event', 'TIPS', {'event_category': '메인페이지', 'event_label': '동물병원 쉽게 데려가는 방법'});
             
         });
 
--- js/main.js
+++ js/main.js
@@ -154,3 +154,22 @@ function getCookie(cName) {
 	}
 	return unescape(cValue);
 }
+
+function shuffle(arr) { // randomly rearanges the items in an array
+	const result = [];
+	for (let i = arr.length-1; i >= 0; i--) {
+		// picks an integer between 0 and i:
+		const r = Math.floor(Math.random()*(i+1));   // NOTE: use a better RNG if cryptographic security is needed
+		// inserts the arr[i] element in the r-th free space in the shuffled array:
+		for(let j = 0, k = 0; j <= arr.length-1; j++) {
+		if(result[j] === undefined) {
+			if(k === r) {
+			result[j] = arr[i];    // NOTE: if array contains objects, this doesn't clone them! Use a better clone function instead, if that is needed. 
+			break;
+			}
+			k++;
+		}
+		}
+	}
+	return result;
+}
